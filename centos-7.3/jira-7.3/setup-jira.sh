9286d43f8ec515b7fce2bf8e276339a47fcc1260
Takayuki Hirota <takayuki.hiro.dev@gmail.com>
2017-01-20 23:03:11 +0900

    CentOS7.3 and JIRA 7.3

---
+++ centos-7.3/jira-7.3/setup-jira.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+yum -y install expect
+
+# install JIRA
+JIRA_BIN=atlassian-jira-software-7.3.0-x64.bin
+wget -nv --no-check-certificate https://www.atlassian.com/software/jira/downloads/binary/$JIRA_BIN
+chmod +x $JIRA_BIN
+
+DIR=$PWD
+
+expect -c "
+set timeout -1
+spawn $DIR/$JIRA_BIN
+expect \"OK\"
+send \"o\n\"
+expect \"Express Install\"
+send \"1\n\"
+expect \"Exit\"
+send \"i\n\"
+expect \"Yes\"
+send \"y\n\"
+expect \"Finishing installation\"
+interact
+"
+# DO NOT insert any entry below.
+# expect script exits unexpectedly.
