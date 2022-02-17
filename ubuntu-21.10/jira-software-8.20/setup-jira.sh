c0914ffce6b98b4134859d9f4801f9d2329e04ba
Takayuki Hirota <takayuki.hiro.dev@gmail.com>
2022-02-17 17:44:21 +0900

    fix #7 support Ubuntu 21.10 and JSW 8.20.5

---
+++ ubuntu-21.10/jira-software-8.20/setup-jira.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt -y install expect
+sudo apt -y install fontconfig
+
+# install JIRA
+JIRA_BIN=atlassian-jira-software-8.20.5-x64.bin
+sudo wget -nv --no-check-certificate https://www.atlassian.com/software/jira/downloads/binary/$JIRA_BIN
+sudo chmod +x $JIRA_BIN
+
+DIR=$PWD
+
+expect -c "
+set timeout -1
+spawn $DIR/$JIRA_BIN
+expect \"OK\"
+send \"o\n\"
+expect \"Express Install\"
+send \"1\n\"
+expect \"Exit\"
+send \"i\n\"
+expect \"Yes\"
+send \"y\n\"
+expect \"Finishing installation\"
+interact
+"
+# DO NOT insert any entry below.
+# expect script exits unexpectedly.
